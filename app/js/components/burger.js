abcf97751c25b81c97d4b0beeb308d9c07e43705
andreytutygin <tutygin.a.v@mail.ru>
2021-01-13 18:38:00 +0300

    Edit

--- app/js/components/burger.js
+++ app/js/components/burger.js
@@ -1,11 +1,11 @@
 document.querySelector('.burger',).addEventListener('click', () => {
     document.querySelector('.burger').classList.toggle('burger_active');
-    document.querySelector('.nav').classList.toggle('nav_active');
+    document.querySelector('.mobile-nav').classList.toggle('mobile-nav_active');
 });
 
-document.querySelector('.nav').addEventListener('click', (e) => {
-    if(e.target.classList.contains('nav__link')) {
+document.querySelector('.mobile-nav').addEventListener('click', (e) => {
+    if(e.target.classList.contains('mobile-nav__link')) {
         document.querySelector('.burger').classList.remove('burger_active');
-        document.querySelector('.nav').classList.remove('nav_active');
+        document.querySelector('.mobile-nav').classList.remove('mobile-nav_active');
     }
 })
