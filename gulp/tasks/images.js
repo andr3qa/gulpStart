773aa2e63cf4fa634dff31fa1b81b0803f2e1453
andr3qa <andr3qaa@gmail.com>
2024-12-23 19:17:22 +0400

    Update

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,9 +1,9 @@
 import gulpif from 'gulp-if';
-import imagemin, {
-  gifsicle,
-  mozjpeg,
-  optipng,
-} from 'gulp-imagemin';
+// import imagemin, {
+//   gifsicle,
+//   mozjpeg,
+//   optipng,
+// } from 'gulp-imagemin';
 import newer from "gulp-newer";
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
@@ -18,17 +18,17 @@ export const images = () => {
         message: "Error: <%= error.message %>"
       })))
     .pipe(newer(app.paths.buildImgFolder))
-    .pipe(gulpif(app.isProd, imagemin([
-      gifsicle({
-        interlaced: true
-      }),
-      mozjpeg({
-        quality: 75,
-        progressive: true
-      }),
-      optipng({
-        optimizationLevel: 2
-      }),
-    ])))
+    // .pipe(gulpif(app.isProd, imagemin([
+    //   gifsicle({
+    //     interlaced: true
+    //   }),
+    //   mozjpeg({
+    //     quality: 75,
+    //     progressive: true
+    //   }),
+    //   optipng({
+    //     optimizationLevel: 2
+    //   }),
+    // ])))
     .pipe(app.gulp.dest(app.paths.buildImgFolder))
 };
